aea0febba558ca5b9efe3554922804fd78cbe91c
Matias Garcia Corral <matihontayt@gmail.com>
2023-12-18 17:31:19 +0100

    nuevo inicio

--- src/Bungalo.java
+++ src/Bungalo.java
@@ -1,31 +1,7 @@
-import java.util.ArrayList;
-public class Bungalo extends Gestionable
+public class Bungalo
 {
-    private String nombre;
-    private int capacidad;
-    private float precio;
-    private ArrayList<ReservaBungalo> reservas;
-
-    public Bungalo(String id, String nombre, int capacidad, float precio)
-    {
-        super(id);
-        this.nombre = nombre;
-        this.capacidad = capacidad;
-        this.precio = precio;
-    }
-
-    public String getNombre()
-    {
-        return this.nombre;
-    }
-
-    public int getCapacidad()
-    {
-        return this.capacidad;
-    }
-
-    public float getPrecio()
-    {
-        return this.precio;
-    }
+    String id;
+    String nombre;
+    int capacidad;
+    float precio;
 }
