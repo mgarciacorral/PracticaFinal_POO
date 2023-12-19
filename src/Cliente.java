14bd89373bf8b40ac18d60b1a600f58835b33510
Matias Garcia Corral <matihontayt@gmail.com>
2023-12-19 12:46:49 +0100

    primer dia, base hecha

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,14 +2,14 @@ import java.util.ArrayList;
 
 public class Cliente
 {
-    private String id;
+    private String identificador;
     private String nombre;
     private String telefono;
     private ArrayList<Factura> facturas;
 
     public Cliente(String id, String nombre, String telefono)
     {
-        this.id = id;
+        this.identificador = id;
         this.nombre = nombre;
         this.telefono = telefono;
     }
